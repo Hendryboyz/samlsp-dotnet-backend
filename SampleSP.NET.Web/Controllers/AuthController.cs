6bb5817d3e67c2f9eaa689930fe69c27732e55fb
Hendryboyz <hendry.js1247@gmail.com>
2021-10-03 15:25:49 +0800

    implement assertion consumer service

--- SampleSP.NET.Web/Controllers/AuthController.cs
+++ SampleSP.NET.Web/Controllers/AuthController.cs
@@ -1,10 +1,13 @@
 using System.Collections.Generic;
+using System.Security.Authentication;
+using System.Threading.Tasks;
 using ITfoxtec.Identity.Saml2;
 using ITfoxtec.Identity.Saml2.MvcCore;
 using ITfoxtec.Identity.Saml2.Schemas;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using SampleSP.NET.Web.Helpers;
 
 namespace SampleSP.NET.Web.Controllers
 {
@@ -26,7 +29,7 @@ namespace SampleSP.NET.Web.Controllers
         {
             var binding = new Saml2PostBinding();
             binding.SetRelayStateQuery(new Dictionary<string, string>() {
-                { RELAY_STATE_RETURN_URL, returnUrl ?? Url.Content("~/greeting") }
+                { RELAY_STATE_RETURN_URL, returnUrl ?? Url.Content("~/auth/greeting") }
             });
             return binding.Bind(new Saml2AuthnRequest(_config)
             {
@@ -35,9 +38,26 @@ namespace SampleSP.NET.Web.Controllers
         }
 
         [HttpGet("assertion-consumer-service")]
-        public IActionResult AssertionConsumerService()
+        public async Task<IActionResult> AssertionConsumerService()
         {
-            return RedirectToAction();
+            var binding = new Saml2PostBinding();
+            var authResponse = new Saml2AuthnResponse(_config);
+
+            binding.ReadSamlResponse(Request.ToGenericHttpRequest(), authResponse);
+            if (authResponse.Status != Saml2StatusCodes.Success)
+            {
+                throw new AuthenticationException($"SAML Response status: {authResponse.Status}");
+            }
+            binding.Unbind(Request.ToGenericHttpRequest(), authResponse);
+
+            await authResponse.CreateSession(HttpContext, claimsTransform: (claimsPrincipal) => ClaimsTransform.Transform(claimsPrincipal));
+            return Redirect(extractRelayState(binding));
+        }
+
+        private string extractRelayState(Saml2PostBinding binding)
+        {
+            var relayState = binding.GetRelayStateQuery();
+            return relayState.ContainsKey(RELAY_STATE_RETURN_URL) ? relayState[RELAY_STATE_RETURN_URL] : Url.Content("~/auth/greeting");
         }
 
         [HttpGet("greeting")]
