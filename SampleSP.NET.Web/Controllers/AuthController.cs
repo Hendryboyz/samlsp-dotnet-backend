794409abc15deb3bcb6a369181af3b2a01599436
Hendryboyz <hendry.js1247@gmail.com>
2021-10-03 15:16:29 +0800

    finish sso entrypoint implementation

--- SampleSP.NET.Web/Controllers/AuthController.cs
+++ SampleSP.NET.Web/Controllers/AuthController.cs
@@ -1,21 +1,24 @@
 using System.Collections.Generic;
 using ITfoxtec.Identity.Saml2;
 using ITfoxtec.Identity.Saml2.MvcCore;
+using ITfoxtec.Identity.Saml2.Schemas;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
 namespace SampleSP.NET.Web.Controllers
 {
     [Route("auth")]
     public class AuthController : Controller
     {
+        private const string RELAY_STATE_RETURN_URL = "ReturnUrl";
         private readonly ILogger<AuthController> _logger;
         private readonly Saml2Configuration _config;
 
-        public AuthController(ILogger<AuthController> logger, Saml2Configuration config)
+        public AuthController(ILogger<AuthController> logger, IOptions<Saml2Configuration> configAccessor)
         {
             _logger = logger;
-            _config = config;
+            _config = configAccessor.Value;
         }
 
         [HttpGet("entrypoint")]
@@ -23,9 +26,12 @@ namespace SampleSP.NET.Web.Controllers
         {
             var binding = new Saml2PostBinding();
             binding.SetRelayStateQuery(new Dictionary<string, string>() {
-                { "relayStateReturnUrl", returnUrl ?? Url.Content("~/greeting") }
+                { RELAY_STATE_RETURN_URL, returnUrl ?? Url.Content("~/greeting") }
             });
-            return binding.Bind(new Saml2AuthnRequest(_config)).ToActionResult();
+            return binding.Bind(new Saml2AuthnRequest(_config)
+            {
+                NameIdPolicy = new NameIdPolicy { AllowCreate = false, Format = "urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress" }
+            }).ToActionResult();
         }
 
         [HttpGet("assertion-consumer-service")]
